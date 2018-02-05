59ed1bd91062c999f4adb3c67e7990d4ce9c782e
USER <USER@DESKTOP-Q2MMLR1>
2018-02-05 10:51:13 +0700

    ByteMaxMin

---
+++ src/bytemaxmin/ByteMaxMin.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package bytemaxmin;
+
+/**
+ *
+ * @author USER
+ */
+public class ByteMaxMin {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        byte minbyte = Byte.MIN_VALUE;
+        byte maxbyte = Byte.MAX_VALUE;
+        System.out.println("Min byte value : " + minbyte);
+        System.out.println("Max byte value : " + maxbyte);
+    }
+    
+}
